0459260bf4be4f12f92d7dd2ae38991bdf26bb58
Aleksandar Mitrovic <aleksa446@gmail.com>
2019-10-04 00:58:41 +0200

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'immutable'
+  ],
+  rules: {
+    'immutable/no-mutation': 2,
+    'indent': [2, "tab"],
+    'no-tabs': 0,
+    'arrow-parens': "off"
+  },
+};
